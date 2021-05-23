e81674319ae3d4376edd40b15fd759189b1ee42f
gautamk1512 <77240480+gautamk1512@users.noreply.github.com>
2021-05-23 08:09:34 +0530

    first commit

---
+++ automass.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time 
+while True:
+    time.sleep(3)
+    pyautogui.typewrite(" happy barthday raj")
+    pyautogui.press("Enter")
+     happy barthday raj
+      happy barthday raj
+       happy barthday raj
+        happy barthday raj
+         happy barthday raj
+          happy barthday raj
+           happy barthday raj
+            happy barthday raj
+             happy barthday raj
+              happy barthday raj
+              
---
+++ automatwhtppsms.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 99109 19556 ', 'happy barthday raj ', 20, 56)
